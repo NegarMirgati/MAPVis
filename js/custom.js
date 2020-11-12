5ad0a873e6d47633250d2e28ddcb942a33eb12ad
Negar <negar.nbj96@yahoo.com>
2020-11-12 14:08:49 +0330

    feat: add mock diagram and P(error) rows

--- js/custom.js
+++ js/custom.js
@@ -21,3 +21,16 @@ var rangeSlider = function () {
 document.addEventListener('DOMContentLoaded', function () {
     rangeSlider();
 }, false);
+
+// This function updates diagram and P(err) values whenever any slider value changes
+var updateDiagram = function () {
+    console.log("update called!")
+    var p_value = document.getElementById('p').value
+    var snr_value = document.getElementById('snr').value;
+    console.log(snr_value)
+    console.log(p_value)
+
+    document.getElementById('err_x0_value').innerText = p_value
+    document.getElementById('err_x1_value').innerText = snr_value;
+
+}

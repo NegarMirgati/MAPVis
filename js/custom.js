881ad99f911de9293e1d26c7421217c51e751c01
Negar <negar.nbj96@yahoo.com>
2020-11-18 15:44:53 +0330

    feat: add live diagram

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+
 var rangeSlider = function () {
     var slider = $('.range-slider'),
         range = $('.range-slider__range'),
@@ -32,10 +33,77 @@ var updateDiagram = function () {
 
     document.getElementById('err_x0_value').innerText = p_value
     document.getElementById('err_x1_value').innerText = snr_value;
-    // #TODO: feed real data to calcPErr func
+    var voltage_x1 = document.getElementById('voltage_1').value
+
     document.getElementById('err_value').innerText = calcPErr(0.1, 0.2)
 
+
+    var data = new google.visualization.DataTable();
+    //var data2 = new google.visualization.DataTable();
+
+    data.addColumn('number', 'X Value');
+    data.addColumn('number', 'Y Value');
+
+
+    function NormalDensityZx(x, Mean, StdDev) {
+
+        var a = x - Mean;
+
+        return Math.exp(-(a * a) / (2 * StdDev * StdDev)) / (Math.sqrt(2 * Math.PI) * StdDev);
+
+    }
+
+    var chartData = new Array([]);
+    chartData.push()
+    var index = 0;
+
+    for (var i = -3; i < 3.1; i += 0.1) {
+
+        chartData[index] = new Array(2);
+
+        chartData[index][0] = i;
+
+        chartData[index][1] = p_value / 0.4 * NormalDensityZx(i, 0, 1);
+
+        index++;
+
+    }
+
+    var chartData2 = new Array([]);
+    chartData2.push()
+    var index2 = 0;
+    for (var i = 1; i < 7.1; i += 0.1) {
+
+        chartData2[index2] = new Array(2);
+
+        chartData2[index2][0] = i;
+
+        chartData2[index2][1] = (1 - p_value) / 0.4 * NormalDensityZx(i, voltage_x1, voltage_x1 / snr_value);
+
+        index2++;
+
+    }
+
+
+    data.addRows(chartData);
+    data.addRows(chartData2);
+
+    options = { width: 500, legend: 'none' };
+
+
+    options.hAxis = {};
+
+    options.hAxis.minorGridlines = {};
+
+    options.hAxis.minorGridlines.count = 12;
+
+    var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+    //chart.draw(data2, options);
 }
+google.load('visualization', '1', { packages: ['corechart'], callback: updateDiagram });
+
 
 // calculate P(error) = P(error|X=0)P(X=0) + P(error|X=1)P(X=1) rounded to 4 digits
 var calcPErr = function (P_err_x0, P_err_x1) {
@@ -57,3 +125,5 @@ var calcSignalPower = function () {
     return power
 
 }
+
+
